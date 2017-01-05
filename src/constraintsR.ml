89ce30d6e57e7fdfaefcb056ad9be4eb3f89cfd6
nekketsuuu <nekketsuuu@users.noreply.github.com>
2017-01-05 15:00:13 +0900

    Modify constraints[T|R].ml: Set.map has a bug in OCaml 4.04.0 It will be fixed in OCaml 4.04.1

--- src/constraintsR.ml
+++ src/constraintsR.ml
@@ -9,21 +9,31 @@ include ConstraintsR
 open Sbst
 
 (* sbst : Type.region sbst -> t -> t *)
-let sbst (sigma : Type.region sbst) c =
+let sbst (sigma : Type.region sbst) cr =
   let sbst'' sigma r =
     try
       List.assoc r sigma
     with
     | Not_found -> r
   in
+  (*
+  (*
+   * Set.map has a bug in OCaml 4.04.0
+   * https://caml.inria.fr/mantis/view.php?id=7403
+   *)
   let sbst' (r1, r2) =
     (sbst'' sigma r1, sbst'' sigma r2)
   in
-  map sbst' c
+  map sbst' cr
+   *)
+  let sbst' (r1, r2) cr =
+    ConstraintsR.add (sbst'' sigma r1, sbst'' sigma r2) cr
+  in
+  ConstraintsR.fold sbst' cr ConstraintsR.empty
 
 (* for debug *)
 let rec to_list c =
-  if is_empty c then []
+  if ConstraintsR.is_empty c then []
   else
-    let elm = choose c in
-    elm :: (to_list @@ remove elm c)
+    let elm = ConstraintsR.choose c in
+    elm :: (to_list @@ ConstraintsR.remove elm c)
--- src/constraintsT.ml
+++ src/constraintsT.ml
@@ -9,15 +9,26 @@ include ConstraintsT
 open Sbst
 
 (* sbst : Type.t sbst -> t -> t *)
-let sbst (sigma : Type.t sbst) c =
+let sbst (sigma : Type.t sbst) ct =
+  (*
+  (*
+   * Set.map has a bug in OCaml 4.04.0
+   * https://caml.inria.fr/mantis/view.php?id=7403
+   *)
   let sbst' (t1, t2) =
     (Type.sbst sigma t1, Type.sbst sigma t2)
   in
-  map sbst' c
+  map sbst' ct
+   *)
+  let sbst' (t1, t2) ct =
+    ConstraintsT.add (Type.sbst sigma t1, Type.sbst sigma t2) ct
+  in
+  ConstraintsT.fold sbst' ct ConstraintsT.empty
 
 (* for debug *)
 let rec to_list c =
-  if is_empty c then []
+  (* print_endline @@ string_of_int @@ cardinal c; *)
+  if ConstraintsT.is_empty c then []
   else
-    let elm = choose c in
-    elm :: (to_list @@ remove elm c)
+    let elm = ConstraintsT.choose c in
+    elm :: (to_list @@ ConstraintsT.remove elm c)
